b078fa0c27da2c51084c5f53f29f6ea534bc734c
Eric Pfleiderer <e.pfleiderer@hotmail.com>
2020-03-19 20:39:15 -0400

    Adding initialization variables.

--- AlphaSilico/src/settings.py
+++ AlphaSilico/src/settings.py
@@ -8,3 +8,8 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 run_folder = ROOT_DIR + '/src/Models/'
 run_archive_folder = ROOT_DIR + '/src/Models/'
+
+
+INITIAL_RUN_NUMBER = 1
+INITIAL_MODEL_VERSION = None
+INITIAL_MEMORY_VERSION = None
