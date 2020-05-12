ff768fc525e5258f57ece92ad0dd1ea0984c1c60
Eric Pfleiderer <e.pfleiderer@hotmail.com>
2020-05-12 12:32:31 -0400

    Updating to dos

--- AlphaSilico/main.py
+++ AlphaSilico/main.py
@@ -15,7 +15,7 @@ from AlphaSilico.src.training import clinical_trial, train
 
 # PRINTOUTS
 # standard_treatment()
-# MCTS_params(simulations=200)
+MCTS_params(simulations=250)
 # MCTS_variance(simulations=200, runs=5)
 
 # agent = Agent(name='Brain', action_size=5, mcts_simulations=config.MCTS_SIMS, cpuct=config.CPUCT, learner=Learner(learning_rate=config.LEARNING_RATE))
@@ -32,21 +32,20 @@ TO DO:
     - Implement dynamic input size for solution vector.  Learner currently initializes with fixed input size, cannot deal with patients with varying tau parameter
         -When done, remove cohort_size=1 from clinical_trial() calls.
         
-    - Optimize MCTS to avoid recomputing previously visited solutions
-    
-    - Parallelisation
-    
     - Establish victory, draw, defeat criteria:
         - Treatment length: 75 days
         - Total simulation length: 180 days (or  360?)
         - Max dose: 4
         
+    - Optimization
+        - Implement dynamic step size 
+        - Optimize MCTS to avoid recomputing previously visited solutions
+        - Parallelisation
         
-brainstorm:
+Brainstorm:
     - Perform criteria analysis 
         -make it harder to win (lower doubling treshold, lower total doses treshold) and see if lerned behavior is different
     -change max dose to 3, 2 and compare learned behaviors
-    
 '''
 
 
